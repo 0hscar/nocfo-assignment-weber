5ef20ebf85e1e04746819fa24d0a7f5a485b1724
Oscar <oscar.weber00@gmail.com>
2024-03-02 14:49:50 +0200

    still in progress

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
+const SortButton = ({name}) => {
+  
+  function sort(){
+
+  }
+  // console.log("tjong" + Object.keys(event))
+
+  return <button>{name}</button>
+}
+
+
+const EventList = ({event}) => {
+  return <div className="eventContainer">
+    <h1>Test</h1>
+    
+    <li>Date: {event.date}</li>
+    <li>Type: {event.type}</li>
+    <p>Priority: {event.priority}</p>
+    <p>Description: {event.description}</p>
+
+  </div>
+
+}
+
 export const Timeline = ({ items }) => {
   // TODO: Replace me
-  return <StyledPlaceholder>your component here</StyledPlaceholder>
+  // console.log(Object.keys(getEvents()))
+  console.log(items)
+  return (
+    <StyledPlaceholder>your component here
+    <br></br>
+    {Object.keys(items[0]).map((value) => <SortButton name={value}></SortButton>)} {/* Creating buttons for every key in objects, assuming every object will contain same keys */}
+    {items.map((value) => <EventList event={value} ></EventList>)} {/* .map() to loop through the events themselves, instead of having to use [1], [2] etc. */}
+    </StyledPlaceholder>
+  ) 
+
+  
+  
 }
+
+
+
 
 const StyledPlaceholder = styled.div`
   padding: 3rem 4rem;
@@ -14,4 +53,6 @@ const StyledPlaceholder = styled.div`
   font-family: Nunito, sans-serif;
   color: #1b98f5;
   text-align: center;
+  max-height: 500px;
+  overflow-y: scroll;
 `
